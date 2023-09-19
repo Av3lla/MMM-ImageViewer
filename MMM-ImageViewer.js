27ba080a726bb2a06205f8265928d4911153022c
Avella <kinggodavella@gmail.com>
2023-09-19 20:44:58 +0900

    Developed By 추가

--- MMM-ImageViewer.js
+++ MMM-ImageViewer.js
@@ -28,6 +28,7 @@ Module.register("MMM-ImageViewer", {
   },
   
   getDom: function() {
+    var mainDiv = document.createElement("div");
     var imageDiv = document.createElement("img");
     imageDiv.className = "image";
     imageDiv.setAttribute('src', `${this.config.url}`);
@@ -35,6 +36,12 @@ Module.register("MMM-ImageViewer", {
     imageDiv.setAttribute('height', `${this.config.height}`);
     imageDiv.setAttribute('alt', 'image not loaded.');
     
-    return imageDiv;
+    var developedbyDiv = document.createElement("div");
+    developedbyDiv.className = "devby";
+    developedbyDiv.innerHTML = "이지원 Github @Av3lla";
+
+    mainDiv.append(imageDiv, developedbyDiv);
+
+    return mainDiv;
   }
 });
